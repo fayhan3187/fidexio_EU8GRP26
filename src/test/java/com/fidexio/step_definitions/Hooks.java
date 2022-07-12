8b0929d847812cddd8283dfb4a51331638550ec5
efelesef <m.kadriefe@gmail.com>
2022-07-12 19:56:52 +0300

    Hooks type  changed

--- src/test/java/com/fidexio/step_definitions/Hooks.java
+++ src/test/java/com/fidexio/step_definitions/Hooks.java
@@ -14,15 +14,23 @@ public class Hooks {
     @After
     public void teardownScenario(Scenario scenario){
 
+        byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot, "image/png", scenario.getName());
         //scenario.isFailed() --> if scenario fails this method will return TRUE boolean value
-        if (scenario.isFailed()){
-            byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", scenario.getName());
-        }
 
 
+//        if (scenario.isFailed()){
+//
+//            byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+//            scenario.attach(screenshot, "image/png", scenario.getName());
+//
+//        }
+
+
+        //BrowserUtils.sleep(5);
         Driver.closeDriver();
 
-
+        //System.out.println("====Closing browser using cucumber @After");
+        //System.out.println("====Scenario ended/ Take screenshot if failed!");
     }
 }
