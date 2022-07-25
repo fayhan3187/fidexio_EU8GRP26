b5c67d686b16c2abee974e9396674c129b6b4f85
zaferizm35 <estat21@gmail.com>
2022-07-25 13:31:38 +0300

    Discard button added 1

--- src/test/java/com/fidexio/runners/CukesRunner.java
+++ src/test/java/com/fidexio/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/fidexio/step_definitions",
         dryRun = false,
-        tags = " @smoke"
+        tags = "@smoke"
         // publish = true //enable a functionality from cucumber to generate a public link for the
                           // report of our final execution of our code
 
